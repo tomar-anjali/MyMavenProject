7cd5caa644333ad580516887a847e18d0956ffcc
Anjali <singhanjali0401@gmail.com>
2024-10-17 14:56:30 +0530

    Pushing my first maven project...!

---
+++ src/main/java/demo/Calculator.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * Hello world!
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("Result: "+a+b);
+    }
+}
