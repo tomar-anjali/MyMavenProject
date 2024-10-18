ab22d7da8bc231212e74fd4c45348d014608913f
Anjali Singh <singhanjali0401@gmail.com>
2024-10-18 13:10:56 +0530

    Update Calculator.java

--- src/main/java/demo/Calculator.java
+++ src/main/java/demo/Calculator.java
@@ -7,6 +7,6 @@ public class Calculator {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        System.out.println("Multiplication Result: "+a+b);
+        System.out.println("Result: "+a+b);
     }
 }
