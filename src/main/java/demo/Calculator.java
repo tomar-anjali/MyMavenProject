d20ac2ae0280e0ada6fc6316ccbb35235772bdb6
Anjali Singh <singhanjali0401@gmail.com>
2024-10-18 13:17:42 +0530

    Update Calculator.java

--- src/main/java/demo/Calculator.java
+++ src/main/java/demo/Calculator.java
@@ -7,6 +7,6 @@ public class Calculator {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        System.out.println("Result: "+a+b);
+        System.out.println("mul Result: "+a+b);
     }
 }
